25dd9dceb55248e76f570fe040e90c5e9096550c
Vidhya Darade <vidhyadarade1@gmail.com>
2023-01-09 14:47:16 +0530

    heap concept

--- Program.cs
+++ Program.cs
@@ -6,11 +6,29 @@ namespace Basics
     {
         static void Main(string[] args)
         {
-            BoxingUnBoxing bu = new BoxingUnBoxing();
-            bu.Boxing();
+            //BoxingUnBoxing bu = new BoxingUnBoxing();
+            //bu.Boxing();
 
-            delegates d = new delegates();
-            d.delegatesE();
+            //delegates d = new delegates();
+            //d.delegatesE();
+
+            //------------------for heap-------------------
+            int[] arr = { 55, 25, 89, 34, 12, 19, 78, 95, 1, 100 };
+            int n = 10, i;
+
+            Console.WriteLine("Heap Sort");
+            Console.Write("Initial array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            heaps heap = new heaps();
+            heap.heapSort(arr, 10);
+            Console.Write("\n Sorted Array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
         }
     }
 }
